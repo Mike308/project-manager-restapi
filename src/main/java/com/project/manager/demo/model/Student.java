24cebc3aad7e49f8a75fd4bd71e4eed9d752fc10
Mike308 <m.werner9205@gmail.com>
2021-03-27 01:28:40 +0100

    added Data annotation

--- src/main/java/com/project/manager/demo/model/Student.java
+++ src/main/java/com/project/manager/demo/model/Student.java
@@ -1,6 +1,7 @@
 package com.project.manager.demo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Table(name = "student")
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Student {
     @Id
     @GeneratedValue
