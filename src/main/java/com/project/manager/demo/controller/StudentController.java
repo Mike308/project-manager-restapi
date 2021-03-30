ed0e3e81f7752c2302f0ceb02327e392f181cd45
Mike308 <m.werner9205@gmail.com>
2021-03-30 21:30:30 +0200

    added student controller

---
+++ src/main/java/com/project/manager/demo/controller/StudentController.java
@@ -0,0 +1,7 @@
+package com.project.manager.demo.controller;
+
+import com.project.manager.demo.model.Student;
+
+public interface StudentController {
+    Student addStudent(Student student);
+}
---
+++ src/main/java/com/project/manager/demo/controller/impl/StudentControllerImpl.java
@@ -0,0 +1,27 @@
+package com.project.manager.demo.controller.impl;
+
+import com.project.manager.demo.controller.StudentController;
+import com.project.manager.demo.model.Student;
+import com.project.manager.demo.service.StudentService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentControllerImpl implements StudentController {
+    private final StudentService studentService;
+
+    public StudentControllerImpl(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @Override
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+}
