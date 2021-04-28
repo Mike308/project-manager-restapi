7cdfb46e653e9aeb79c8606326b078aa657abfd4
mikolaj.werner <mikolaj.werner@utp.edu.pl>
2021-04-28 12:48:34 +0200

    added repository annotation

--- src/main/java/com/project/manager/demo/repository/StudentRepository.java
+++ src/main/java/com/project/manager/demo/repository/StudentRepository.java
@@ -2,7 +2,9 @@ package com.project.manager.demo.repository;
 
 import com.project.manager.demo.model.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
     Student findByIndexNumber(String indexNumber);
 }
